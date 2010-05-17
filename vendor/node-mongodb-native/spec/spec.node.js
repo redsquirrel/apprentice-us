2d516521ed0202edc3cb53dc473626c76e12995d
Dave Hoover <dave.hoover@gmail.com>
2010-05-17 00:46:46 -0500

    Adding mongo back in, as a non-submodule

---
+++ vendor/node-mongodb-native/spec/spec.node.js
@@ -0,0 +1,27 @@
+require.paths.unshift("./spec/lib");
+var sys = require('sys');
+for (var key in sys)
+	GLOBAL[key] = sys[key];
+
+mongo = require("../lib/mongodb")
+
+require("jspec")
+
+var posix = require('fs')
+
+quit = process.exit
+print = puts
+
+readFile = function(path) {
+  return posix.readFileSync(path);
+}
+
+if (process.ARGV[2])
+  JSpec.exec('spec/spec.' + process.ARGV[2] + '.js')  
+else
+  JSpec
+    .exec('spec/spec.bson.js')
+    .exec('spec/spec.commands.js')
+JSpec.run({ reporter: JSpec.reporters.Terminal, failuresOnly: true })
+JSpec.report()
+
