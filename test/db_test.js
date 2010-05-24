c77237c7df258262a501baf4a5c82c01f4834e3b
Dave Hoover <dave.hoover@gmail.com>
2010-05-24 15:18:18 -0500

    Testing deeper into the loadResource/loadFromQuery functions

--- test/db_test.js
+++ test/db_test.js
@@ -5,15 +5,81 @@ var sys = require('sys')
 var it = function(description, test) {
   test()
 }
-
-it("uses the name attribute to figure out which collection to look up", function() {
-
-  var dbStub = {
-    collection: function(collectionName) {
-      assert.equal(collectionName, "apprentices")
+var dbStub = function(collection) {
+  return {
+    collection: function(collectionName, collectionCallback) {
+      collectionCallback(undefined, collection)
     }
   }
+}
+var connect = function(dbStub) {
+  return function(callback) { callback(dbStub) }
+}
 
-  var connection = db.create(function(callback) { callback(dbStub) })
+it("uses the name attribute to figure out which collection to look up", function() {
+  var assertion = function(collectionName) {
+    assert.equal(collectionName, "apprentices")
+  }
+
+  var connection = db.create(connect({ collection: assertion }))
   connection.loadResource({name: "apprentices", id: "ethan_gunderson"})
+})
+
+it("uses the name id to figure out which document to look up", function() {
+  var assertion = function(filter) {
+    assert.deepEqual(filter, {slug: "ethan_gunderson"})
+  }
+
+  var connection = db.create(connect(dbStub({find: assertion})))
+  connection.loadResource({name: "apprentices", id: "ethan_gunderson"})
+})
+
+it("provides apprentice data to the onLoad callback", function() {
+  var assertion = function(resourceData) {
+    assert.deepEqual(resourceData.results, { foo: 'bar', shop: { foo: 'bar' } })
+  }
+  
+  var cursorStub = {
+    nextObject: function(cursorCallback) {
+      cursorCallback(undefined, {foo: "bar"})
+    }
+  }
+  
+  var collectionStub = {
+    find: function(ignored, ignored, findCallback) {
+      findCallback(undefined, cursorStub)
+    }    
+  }
+  
+  var connection = db.create(connect(dbStub(collectionStub)))
+  connection.loadResource({name: "apprentices", id: "ethan_gunderson"}, assertion)
+})
+
+it("provides shop data to the onLoad callback", function() {
+  var expectedShopName = "obtiva"
+  var expectedApprentices = [{name: "colin"}, {name: "ethan"}]
+  
+  var assertion = function(resourceData) {
+    assert.equal(resourceData.results.name, expectedShopName)
+    assert.deepEqual(resourceData.results.apprentices, expectedApprentices)
+  }
+  
+  var cursorStub = {
+    nextObject: function(cursorCallback) {
+      cursorCallback(undefined, {name: expectedShopName})
+    },
+    
+    toArray: function(cursorCallback) {
+      cursorCallback(undefined, expectedApprentices)
+    }
+  }
+  
+  var collectionStub = {
+    find: function(ignored, ignored, findCallback) {
+      findCallback(undefined, cursorStub)
+    }    
+  }
+
+  var connection = db.create(connect(dbStub(collectionStub)))
+  connection.loadResource({name: "shops", id: "obtiva"}, assertion)
 })
