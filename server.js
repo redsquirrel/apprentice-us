70b3c629f06aafcf8ee5675195a4c517003d20fc
Dave Hoover <dave.hoover@gmail.com>
2010-05-26 13:06:09 -0500

    Killing the mongo module and moving the connect function back into db.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var db = require('./lib/db')
 var http = require('./lib/http')
-var mongo = require('./lib/mongo')
 var public = require('./lib/public')
 var renderer = require('./lib/renderer')
 var resourceful = require('./lib/resourceful')
 var sys = require('sys')
 
-var connection = db.create(mongo.connect)
+var connection = db.create(db.connect)
 
 http.serve(function (request, response) {
   sys.puts(sys.inspect(request))
